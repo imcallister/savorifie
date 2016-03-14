6707b92de527e181d7da52f01ab34575d0996ac6
imcallister <ian.mcallister@electronifie.com>
2016-03-13 22:19:55 -0400

    remove cleanlogs from base

--- savor/base/urls.py
+++ savor/base/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = patterns('',
  
-    url(r'^cleanlogs/$', views.cleanlogs, name='cleanlogs'),
     url(r'^download_expenses/$', views.output_expenses),
     url(r'base/api/(?P<api_view>[_a-zA-Z0-9]+)/$', views.api),
     url(r'^dump_fixtures/$', views.dump_fixtures),
