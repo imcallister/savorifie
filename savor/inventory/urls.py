85abbdc07b6f6c1d42a3f959a43003befd32c919
imcallister <ian.mcallister@electronifie.com>
2016-05-06 18:40:22 -0400

    add make_batch

--- savor/inventory/urls.py
+++ savor/inventory/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     url(r'inventory/reconcile_warehouse/$', 'inventory.views.reconcile_warehouse'),
     url(r'inventory/request_fulfill/(?P<order_id>[_a-zA-Z0-9]+)/$', 'inventory.views.request_fulfill'),
     url(r'inventory/thoroughbred_upload/$', 'inventory.views.thoroughbred_upload'),
+    url(r'inventory/make_batch/(?P<warehouse>[_a-zA-Z0-9]+)/$', 'inventory.views.make_batch'),
     url(r'^inventory/upload/(?P<file_type>.*)/$', 'inventory.views.upload_file', name='upload_file'),
 
     url(r'^inventory/management/$', 'inventory.views.management'),
