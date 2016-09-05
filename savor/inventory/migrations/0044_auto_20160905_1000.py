e729574d43c8eb025a3b24c4595e07de6abeb6a9
imcallister <ian.mcallister@electronifie.com>
2016-09-05 13:35:54 -0400

    change order of migrations

--- savor/inventory/migrations/0044_auto_20160905_1000.py
+++ savor/inventory/migrations/0044_auto_20160905_1000.py
@@ -8,6 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('inventory', '0043_remove_channelshipmenttype_channel'),
+        ('sales', '0002_auto_20160905_0942'),
     ]
 
     operations = [
