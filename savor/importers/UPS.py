e9c81c1511c5fbd180ad3913bf988b3d0562ccb2
imcallister <ian.mcallister@electronifie.com>
2016-09-13 09:13:12 -0400

    agg UPS charges by tracking number

--- savor/importers/UPS.py
+++ savor/importers/UPS.py
@@ -1,4 +1,5 @@
 import os
+from itertools import groupby
 
 from django.conf import settings
 from django.contrib import messages
@@ -19,6 +20,14 @@ logger = logging.getLogger('default')
 DATA_ROOT = getattr(settings, 'DATA_DIR', os.path.join(settings.ENVIRON_DIR, 'data'))
 INCOMING_ROOT = os.path.join(DATA_ROOT, 'incoming')
 PROCESSED_ROOT = os.path.join(DATA_ROOT, 'processed')
+
+
+def agg_tracknum(tn_list):
+    return {'ship_date': tn_list[0]['ship_date'],
+            'invoice_number': tn_list[0]['invoice_number'],
+            'account': tn_list[0]['account'],
+            'charge': sum([s['charge'] for s in tn_list]),
+            'tracking_number': tn_list[0]['tracking_number']}
 
 
 def order_upload(request):
@@ -47,16 +56,24 @@ def order_upload(request):
 
 def process_ups(file_name):
     incoming_name = os.path.join(INCOMING_ROOT, file_name)
-    ship_charges, errors = UPSCSVModel.import_data(data=open(incoming_name, 'rU'), skip_rows=6)
+    ship_charges, errors = UPSCSVModel.import_data(data=open(incoming_name, 'rU'),
+                                                   skip_rows=6)
     shipper_id = inventory_api.shipper('UPS', {})['id']
+
+    # group the data by unique tracking numbers
+    ship_charges = sorted(ship_charges, key=lambda x: x['tracking_number'])
+    gpd_ship_charges = groupby(ship_charges, key=lambda x: x['tracking_number'])
+    aggd_ship_charges = [agg_tracknum(list(v)) for k, v in gpd_ship_charges]
 
     new_recs_ctr = 0
     exist_recs_ctr = 0
     errors_cnt = len(errors)
 
-    for rec in ship_charges:
+    for rec in aggd_ship_charges:
         rec['shipper_id'] = shipper_id
-        rec_obj = None
+        rec_obj = ShippingCharge.objects \
+                                .filter(tracking_number=rec['tracking_number']) \
+                                .first()
 
         if rec_obj:
             # if warehose fulfill object already exists ... skip to next one
