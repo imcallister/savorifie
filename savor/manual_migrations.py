e7508a1a0b40bbf1bc06140145e1e0fc7373fe79
imcallister <ian.mcallister@electronifie.com>
2016-09-22 10:43:55 -0400

    shipping charges from NC2

---
+++ savor/manual_migrations.py
@@ -0,0 +1,35 @@
+import logging
+
+from sales.models import Sale
+from fulfill.models import WarehouseFulfill
+from sales.importers.shopify import shopify_fee
+
+from importers.NC2 import create_nc2_shippingcharge
+
+logger = logging.getLogger('default')
+
+def add_shopify_fees():
+    for s in Sale.objects.filter(channel__label='SHOPIFY'):
+        s.channel_charges = shopify_fee(s)
+        s.save()
+    return
+
+def backfill_nc2_shipcharges():
+    new_charges = 0
+    duplicated_not_saved = 0
+    unknown = 0
+    for wf in WarehouseFulfill.objects \
+                              .filter(warehouse__label='NC2') \
+                              .filter(fulfillment__ship_type__label='IFS_BEST'):
+        ret_code = create_nc2_shippingcharge(wf)
+        if ret_code == 'SHIPPING CHARGE ALREADY EXISTS':
+            duplicated_not_saved += 1
+        elif ret_code == 'SHIPPING CHARGE CREATED':
+            new_charges += 1
+        else:
+            unknown += 1
+
+    logger.info('Manual Migration: backfill_nc2_shipcharges')
+    logger.info('%s new shipping charges, %s duplicated not saved, %s unknown' \
+                % (new_charges, duplicated_not_saved, unknown))
+    return
--- savor/sales/manual_migrations.py
+++
@@ -1,9 +0,0 @@
-from models import Sale
-from importers.shopify import shopify_fee
-
-def add_shopify_fees():
-    for s in Sale.objects.filter(channel__label='SHOPIFY'):
-        s.channel_charges = shopify_fee(s)
-        s.save()
-    return
-
