6179d298307ab2bcf7786e4c3183f16f5543f41b
imcallister <ian.mcallister@electronifie.com>
2016-09-26 12:18:44 -0400

    add salestax migration

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -2,6 +2,7 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var App = require('./app')
 var BHelp = require('./help') 
+//var DataTable = require('./fdatatable') 
 
 ReactDOM.render(<BHelp/>, document.getElementById('the-help'));
 ReactDOM.render(<App/>, document.getElementById('the-help2'));
