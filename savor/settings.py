c54e3a47d9c7655cd7f60aa75181bb2d1047bdf1
Ian McAllister <ian@mcallisternevins.com>
2016-10-03 19:07:38 -0400

    manage 1.8 and 1.9

--- savor/settings.py
+++ savor/settings.py
@@ -4,6 +4,13 @@ import sys
 import pandas
 
 
+from django import get_version as django_version
+from distutils.version import StrictVersion
+DJANGO_19 = StrictVersion(django_version()) >= StrictVersion('1.9')
+DJANGO_18 = not DJANGO_19
+
+
+
 # CELERY SETUP
 import djcelery
 djcelery.setup_loader()
