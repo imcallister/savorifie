2423a39bb725e4738a08be81b6d7661ebb781454
Ian McAllister <ian@mcallisternevins.com>
2016-10-10 21:11:25 -0400

    update celery task example

--- savor/base/views/task_views.py
+++ savor/base/views/task_views.py
@@ -8,21 +8,6 @@ from django.template import RequestContext
 from django.http import JsonResponse
 
 from accountifie.celery import background_task
-#from accountifie.celery.celery_main import app as celery_app
-
-
-"""
-from celery import shared_task
-
-
-@shared_task
-def tri_11(request):
-    tot = 0
-    for i in range(10):
-        sleep(1)
-        tot += i
-    return tot
-"""
 
 # long-running calc
 def tri_10():
@@ -39,7 +24,7 @@ def long_calc2(request):
 
 
 def long_calc_task(request):
-    task_name = 'long_calc2-%s' % datetime.datetime.now().isoformat()
+    task_name = 'long_calc2'
     task_id = background_task(task=task_name, calc=tri_10).id
     status_url = 'background_task/status/%s' % task_id
     return JsonResponse({'task_id': task_id, 'task_name': task_name, 'status_url': status_url})
