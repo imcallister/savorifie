d24d3b3fd8afca7b02aec94f360fa0a7751cec5a
Ian McAllister <ian@mcallisternevins.com>
2016-10-15 11:53:58 -0400

    re-doing FRB importer

---
+++ savor/importers/FRB.py
@@ -0,0 +1,54 @@
+import os
+
+from django.conf import settings
+
+from accountifie.common.uploaders.upload_tools import order_upload
+from accountifie.gl.models import ExternalAccount
+from base.models import Cashflow
+
+from .file_models.FRB import FRBCSVModel
+import logging
+logger = logging.getLogger('default')
+
+
+DATA_ROOT = getattr(settings, 'DATA_DIR', os.path.join(settings.ENVIRON_DIR, 'data'))
+INCOMING_ROOT = os.path.join(DATA_ROOT, 'incoming')
+PROCESSED_ROOT = os.path.join(DATA_ROOT, 'processed')
+
+
+def upload(request):
+    processor = process_frb
+    return order_upload(request,
+                        processor,
+                        label=False)
+
+
+def process_frb(file_name):
+    incoming_name = os.path.join(INCOMING_ROOT, file_name)
+    cf_records, errors = FRBCSVModel.import_data(data=open(incoming_name, 'rU'), skip_rows=3)
+    
+    new_recs_ctr = 0
+    exist_recs_ctr = 0
+    errors_cnt = len(errors)
+
+    # HARDCODE
+    ext_account = ExternalAccount.objects.get(gl_account__id='1001').id
+
+    for rec in cf_records:
+        rec['amount'] = rec['debit'] + rec['credit']
+        rec_obj = Cashflow.objects \
+                          .filter(post_date=rec['post_date']) \
+                          .filter(amount=rec['amount']) \
+                          .filter(external_id=rec['external_id']) \
+                          .count()
+        if rec_obj > 0:
+            exist_recs_ctr += 1
+        else:
+            new_recs_ctr += 1
+            rec['ext_account'] = ext_account
+            rec_obj = Cashflow(**rec)
+            rec_obj.save()
+
+    summary_msg = 'Loaded FRB file: %d new records, %d duplicate records, %d bad rows' \
+                                    % (new_recs_ctr, exist_recs_ctr, errors_cnt)
+    return summary_msg, errors
--- savor/importers/urls.py
+++ savor/importers/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 import UPS
 import NC2
+import FRB
 
 urlpatterns = [
     url(r'importers/nc2_upload/$', NC2.order_upload),
-    url(r'importers/ups_upload/$', UPS.order_upload)
+    url(r'importers/ups_upload/$', UPS.order_upload),
+    url(r'importers/upload/frb/$', FRB.upload),
 ]
