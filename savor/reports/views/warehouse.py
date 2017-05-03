8dbf2fc621a6b21e28d857c95820f526ccda9276
Ian McAllister <ian@mcallisternevins.com>
2017-05-02 22:53:59 -0400

    fix uploader

--- savor/reports/views/warehouse.py
+++ savor/reports/views/warehouse.py
@@ -7,13 +7,14 @@ from django.template import RequestContext
 from django.utils.safestring import mark_safe
 
 from accountifie.common.table import get_table
+from accountifie.toolkit.forms import FileForm
 import fulfill.apiv1 as flfl_api
 
 
 
 @login_required
 def warehouse_rec(request):
-    context = {}
+    context = {'shopify_upload_form': FileForm()}
     context['NC2_unreconciled'] = get_table('no_warehouse_record')(warehouse='NC2')
     context['152Frank_unreconciled'] = get_table('no_warehouse_record')(warehouse='152Frank')
 
