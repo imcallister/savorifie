01381807e305f112a621004e2479f05d853c2013
Ian McAllister <ian@mcallisternevins.com>
2017-05-10 16:46:17 -0400

    menu bars

--- client/app.jsx
+++ client/app.jsx
@@ -23,8 +23,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 
-import savorLogo from './shared/images/savor-logo.png';
 import MenuBar from './shared/components/MenuBar';
+import MenuBar2 from './shared/components/MenuBar2';
 
 import './styles/main.scss';
 
@@ -34,13 +34,6 @@ const styles = {
   },
 };
 
-const routeLabels = {
-    '': 'Home',
-    'fulfillment': 'Fulfillment',
-    'inventory': 'Inventory',
-    'analysis': 'Analysis',
-    'logout': 'Logout'
-}
 
 class App extends React.Component {
 
@@ -58,90 +51,15 @@ class App extends React.Component {
         };
     }
 
-    handleRouteChange = (event, index) => {
-        console.log('handling Route Change', index);
-        this.setState({route: index});
-        this.props.dispatch(push('/' + index));
-    };
-
-
-    goToIndex = () => {
-        this.props.dispatch(push('/'));
-    };
-
-    goToFulfillment = () => {
-        this.props.dispatch(push('/fulfillment'));
-    };
-
-    goToInventory = () => {
-        this.props.dispatch(push('/inventory'));
-    };
-
-    goToAnalysis = () => {
-        this.props.dispatch(push('/analysis'));
-    };
-
-    
-    logout = () => {
-        this.props.dispatch(authLogoutAndRedirect());
-    };
-
-
     render() {
         const { actions } = this.props;
-        
-        const homeClass = classNames({
-            active: this.props.pathName === '/'
-        });
-
-        const fulfillmentClass = classNames({
-            active: this.props.pathName === '/fulfillment'
-        });
-
-        const loginClass = classNames({
-            active: this.props.pathName === '/login'
-        });
-
         return (
             <div>
-                <MenuBar/>
+                <MenuBar2/>
                 <MuiThemeProvider>
                     <div>
-                        <Toolbar>
-                            <ToolbarGroup firstChild={true}>
-                                {this.props.isAuthenticated ?
-                                    <div>
-                                    <IconMenu
-                                      iconButtonElement={<IconButton><MenuIcon /></IconButton>}
-                                      onChange={this.handleRouteChange}
-                                      value={this.state.valueSingle}
-                                    >
-                                        <MenuItem value="" primaryText="Home" />
-                                        <MenuItem value="fulfillment" primaryText="Fulfillment" />
-                                        <MenuItem value="inventory" primaryText="Inventory" />
-                                        <MenuItem value="analysis" primaryText="Analysis" />
-                                        <MenuItem value="logout" primaryText="Logout" />
-                                    </IconMenu>
-                                    <ToolbarTitle text={routeLabels[this.state.route]} />
-                                    </div>
-                                    :
-                                    null}
-                            </ToolbarGroup>
-                            <ToolbarGroup>
-                              <img className="page-logo margin-bottom-medium margin-top-medium"
-                                     src={savorLogo}
-                                     style={styles.mediumSize}
-                                     alt="Savor"
-                                />
-                            </ToolbarGroup>
-                            
-                          </Toolbar>
-
-                        <div>
-                            {this.props.children}
-                        </div>
+                        {this.props.children}
                     </div>
-                    
                 </MuiThemeProvider>
             </div>
         );
---
+++ client/shared/components/MenuBar2.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
+import { authLogoutAndRedirect } from '../../actions/auth';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
+
+import savorLogo from '../images/savor-logo.png';
+
+import '../../styles/main.scss';
+
+const styles = {
+  mediumSize: {
+    height: '80%',
+  },
+};
+
+const routeLabels = {
+    '': 'Home',
+    'fulfillment': 'Fulfillment',
+    'inventory': 'Inventory',
+    'analysis': 'Analysis',
+    'logout': 'Logout'
+}
+
+
+class MenuBar2 extends Component {
+
+      constructor(props) {
+        super(props);
+        this.state = {open: false};
+      }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
+    handleRouteChange = (event, index) => {
+        if (index == 'logout') {
+            this.props.dispatch(authLogoutAndRedirect());
+        }
+        else {
+            this.setState({route: index});
+            this.props.dispatch(push('/' + index));    
+        }
+    };
+
+	render() {
+        return (
+                <MuiThemeProvider>
+                    <div>
+                        <Toolbar>
+                            <ToolbarGroup firstChild={true}>
+                                {this.props.isAuthenticated ?
+                                    <div>
+                                    <IconMenu
+                                      iconButtonElement={<IconButton><MenuIcon /></IconButton>}
+                                      onChange={this.handleRouteChange}
+                                      value={this.state.valueSingle}
+                                    >
+                                        <MenuItem value="" primaryText="Home" />
+                                        <MenuItem value="fulfillment" primaryText="Fulfillment" />
+                                        <MenuItem value="inventory" primaryText="Inventory" />
+                                        <MenuItem value="analysis" primaryText="Analysis" />
+                                        <MenuItem value="logout" primaryText="Logout" />
+                                    </IconMenu>
+                                    <ToolbarTitle text={routeLabels[this.state.route]} />
+                                    </div>
+                                    :
+                                    <ul className="nav navbar-nav navbar-right">
+                                        <li className={this.props.loginClass}>
+                                            <Link className="js-login-button" to="/login">Login</Link>
+                                        </li>
+                                    </ul>
+                                }
+                            </ToolbarGroup>
+                            <ToolbarGroup>
+                              <img className="page-logo margin-bottom-medium margin-top-medium"
+                                     src={savorLogo}
+                                     style={styles.mediumSize}
+                                     alt="Savor"
+                                />
+                            </ToolbarGroup>
+                            
+                          </Toolbar>
+                    </div>
+                    
+                </MuiThemeProvider>
+        );
+	}
+}
+
+function mapStateToProps(state, ownProps) {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+    };
+}
+
+
+export default connect(mapStateToProps)(MenuBar2);		
